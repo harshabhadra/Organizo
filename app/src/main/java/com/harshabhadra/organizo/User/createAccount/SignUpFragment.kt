91bbfbc2cccda3c2b7f35b53c9e206097196545d
harshabhadra <harshasharkey@gmail.com>
2020-02-18 11:17:28 +0530

    SignUpFragment Bug solved

--- app/src/main/java/com/harshabhadra/organizo/User/createAccount/SignUpFragment.kt
+++ app/src/main/java/com/harshabhadra/organizo/User/createAccount/SignUpFragment.kt
@@ -1,18 +1,16 @@
 package com.harshabhadra.organizo.User.createAccount
 
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.harshabhadra.organizo.MainActivity
-
-import com.harshabhadra.organizo.R
+import androidx.navigation.fragment.findNavController
 import com.harshabhadra.organizo.databinding.FragmentSignUpBinding
 
 /**
@@ -20,37 +18,68 @@ import com.harshabhadra.organizo.databinding.FragmentSignUpBinding
  */
 class SignUpFragment : Fragment() {
 
-    private lateinit var signUpViewModel:SignUpViewModel
+    private lateinit var signUpViewModel: SignUpViewModel
+    private lateinit var name: String
+    private lateinit var signUpBinding: FragmentSignUpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val signUpBinding = FragmentSignUpBinding.inflate(inflater,container,false)
+        signUpBinding = FragmentSignUpBinding.inflate(inflater, container, false)
 
-        val email = signUpBinding.signUpEmailTextInput.text.toString()
-        val password = signUpBinding.signUpPasswordTextInput.text.toString()
-        if(activity != null) {
+        if (activity != null) {
             val viewModelFactory = SignUpViewModelFactory(activity!!)
             //Initializing the SignUpViewModel class
-            signUpViewModel = ViewModelProvider(this,viewModelFactory).get(SignUpViewModel::class.java)
-
+            signUpViewModel =
+                ViewModelProvider(this, viewModelFactory).get(SignUpViewModel::class.java)
         }
 
+        //Create Account on signUp button Clicked
         signUpBinding.signUpButton.setOnClickListener {
-            signUpViewModel.createAccount(email, password)
+            val email = signUpBinding.signUpEmailTextInput.text.toString()
+            val password = signUpBinding.signUpPasswordTextInput.text.toString()
+            val confirmPassword = signUpBinding.signUpConfirmPasswordTextInput.text.toString()
+            name = signUpBinding.signUpNameTextinput.text.toString()
+            verifyCreandCreateAccount(name,email,password,confirmPassword)
         }
-        signUpViewModel.user.observe(viewLifecycleOwner, Observer {
-            it?.let {
-//                val intent = Intent(activity,MainActivity::class.java)
-//                startActivity(intent)
-                Toast.makeText(context,"Welcome ${it.email}",Toast.LENGTH_SHORT).show()
 
-            }?:let {
-                Toast.makeText(context,"Unable to create User",Toast.LENGTH_SHORT).show()
+        //Set OnclickListener to already user button
+        signUpBinding.alreadyUserButton.setOnClickListener {
+            findNavController().navigate(SignUpFragmentDirections.actionSignUpFragmentToLogInFragment())
+        }
+
+        //Observing user LiveData
+        signUpViewModel.user.observe(viewLifecycleOwner, Observer { user ->
+            user?.let {
+                user.displayName?.let {
+                    Toast.makeText(context, "Welcome ${user.displayName}", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
         })
+        
         return signUpBinding.root
+    }
+
+    fun verifyCreandCreateAccount(
+        name: String,
+        email: String,
+        password: String,
+        conPassword: String
+    ){
+        if (!(TextUtils.isEmpty(name)) && signUpViewModel.verifyEmail(email) && signUpViewModel.validatePassword(
+            password,
+            conPassword
+        )){
+            signUpViewModel.createAccount(name, email, password)
+        }else if (!(TextUtils.isEmpty(name))){
+            signUpBinding.signUpNameTextinput.setError("Enter Name")
+        }else if (!signUpViewModel.verifyEmail(email)){
+            signUpBinding.signUpEmailTextInput.setError("Enter a valid Email")
+        }else if (!signUpViewModel.validatePassword(password, conPassword)){
+            signUpBinding.signUpPasswordTextInput.setError("Password Mismatch")
+        }
     }
 
     override fun onStart() {
--- app/src/main/java/com/harshabhadra/organizo/User/createAccount/SignUpViewModel.kt
+++ app/src/main/java/com/harshabhadra/organizo/User/createAccount/SignUpViewModel.kt
@@ -1,46 +1,100 @@
 package com.harshabhadra.organizo.User.createAccount
 
 import android.app.Activity
-import android.content.Context
+import android.text.TextUtils
 import android.util.Log
+import android.util.Patterns
 import android.widget.Toast
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import kotlinx.coroutines.*
 
 
 private lateinit var auth: FirebaseAuth
+
 class SignUpViewModel(private val context: Activity) : ViewModel() {
 
     init {
         auth = FirebaseAuth.getInstance()
     }
 
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    //Initializing FireBaseUser
     private var _user = MutableLiveData<FirebaseUser?>()
-    val user:LiveData<FirebaseUser?>
-    get() = _user
+    val user: LiveData<FirebaseUser?>
+        get() = _user
 
     //Function to get currentUser
-    fun getCurrentUser(){
+    fun getCurrentUser() {
         _user.value = auth.currentUser
     }
 
-    fun createAccount(email:String = "harshasharkey@gmail.com", password:String = "123" ){
-        auth.createUserWithEmailAndPassword(email,password)
-            .addOnCompleteListener(context){task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("SignUpViewModel", "createUserWithEmail:success")
-                     _user.value = auth.currentUser
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("SingUpViewModel", "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(context, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                }
+    //Creating Account with email and password
+    fun createAccount(name: String, email: String, password: String) {
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(context) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.e("SignUpViewModel", "createUserWithEmail:success")
+                            _user.value = auth.currentUser
+                            updateProfileInfo(name,_user.value)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.e("SingUpViewModel", "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                context, "Authentication failed.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
             }
+        }
+    }
+
+    //validate email
+    fun verifyEmail(email: CharSequence):Boolean{
+        return ((!TextUtils.isEmpty(email)) && Patterns.EMAIL_ADDRESS.matcher(email).matches())
+    }
+
+    //validate password
+    fun validatePassword(password: String, conPassword:String):Boolean{
+        return ((!TextUtils.isEmpty(password) && !(TextUtils.isEmpty(conPassword)&& (password.length>=6) && password.equals(conPassword))))
+    }
+
+    //Update user profile
+    fun updateProfileInfo(userName: String, user: FirebaseUser?) {
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                val profileUpdates = UserProfileChangeRequest.Builder()
+                    .setDisplayName(userName)
+                    .build()
+
+                user?.updateProfile(profileUpdates)
+                    ?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            _user.value = auth.currentUser
+                        } else {
+                            Toast.makeText(
+                                context, "Cannot Update User Profile",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
