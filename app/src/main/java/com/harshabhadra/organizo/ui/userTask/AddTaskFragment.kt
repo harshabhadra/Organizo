fa2aa2d3fa6323455bda7ceb546fe3319f62eba7
harshabhadra <harshasharkey@gmail.com>
2020-02-21 20:06:23 +0530

    Changing time and date textView by viewModel

--- app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskFragment.kt
+++ app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import com.harshabhadra.organizo.R
 
 import com.harshabhadra.organizo.Viewanimation.initView
@@ -17,6 +18,8 @@ import com.harshabhadra.organizo.Viewanimation.showIn
 import com.harshabhadra.organizo.Viewanimation.showOut
 import com.harshabhadra.organizo.databinding.FragmentAddTaskBinding
 import kotlinx.android.synthetic.main.add_task_body.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -25,6 +28,7 @@ class AddTaskFragment : Fragment() {
 
     private lateinit var addTaskBinding: FragmentAddTaskBinding
     private var isStart:Boolean = false
+    private lateinit var addTaskViewModel: AddTaskViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,13 +37,26 @@ class AddTaskFragment : Fragment() {
         // Inflate the layout for this fragment
         addTaskBinding = FragmentAddTaskBinding.inflate(inflater, container, false)
 
+        val application = requireNotNull(this.activity).application
+        //Initializing ViewModel class
+        val addTaskViewModelFactory = AddTaskViewModelFactory(application)
+        addTaskViewModel = ViewModelProvider(this,addTaskViewModelFactory).get(AddTaskViewModel::class.java)
+
         //Check what to do Start or save task
         isStart = arguments?.getBoolean("isStart")!!
 
         if (!isStart){
-            addTaskBinding.addTaskBody.end_time_card.visibility = View.VISIBLE
+            addTaskBinding.addTaskBody.endTimeCard.visibility = View.VISIBLE
             addTaskBinding.chooseFab.setImageDrawable(context?.let {getDrawable(it,R.drawable.ic_save_black_24dp) })
+            addTaskBinding.addTaskBody.startTimeChangeButton.visibility = View.VISIBLE
         }
         return addTaskBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        addTaskBinding.addTaskViewModel = addTaskViewModel
+        addTaskViewModel.getTime()
+        addTaskViewModel.getDate()
     }
 }
--- app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskViewModel.kt
+++ app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskViewModel.kt
@@ -2,8 +2,40 @@ package com.harshabhadra.organizo.ui.userTask
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class AddTaskViewModel(private val context: Application) : AndroidViewModel(context) {
 
 
+    private var _dateString = MutableLiveData<String>()
+    val dateString: LiveData<String>
+        get() = _dateString
+
+    private var _timeString = MutableLiveData<String>()
+    val timeString: LiveData<String>
+        get() = _timeString
+
+    //Get Current Time
+    fun getTime(){
+        val date = getCurrentDateTime()
+        _timeString.value = date.toString("hh:mm aa",Locale.getDefault())
+    }
+
+    //Get current Date
+    fun getDate(){
+        val date = getCurrentDateTime()
+        _dateString.value = date.toString("E, dd MMM yyyy")
+    }
+    private fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
+        val formatter = SimpleDateFormat(format, locale)
+        return formatter.format(this)
+    }
+
+    private fun getCurrentDateTime(): Date {
+        return Calendar.getInstance().time
+    }
 }
