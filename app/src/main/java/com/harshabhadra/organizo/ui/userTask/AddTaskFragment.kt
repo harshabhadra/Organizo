4e64866aaf444611d3f8d885383eb41e3d753233
harshabhadra <harshasharkey@gmail.com>
2020-02-21 20:21:02 +0530

    Date and Time Picker Added

--- app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskFragment.kt
+++ app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskFragment.kt
@@ -2,29 +2,23 @@ package com.harshabhadra.organizo.ui.userTask
 
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
-import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.harshabhadra.organizo.R
-
-import com.harshabhadra.organizo.Viewanimation.initView
-import com.harshabhadra.organizo.Viewanimation.rotateFab
-import com.harshabhadra.organizo.Viewanimation.showIn
-import com.harshabhadra.organizo.Viewanimation.showOut
 import com.harshabhadra.organizo.databinding.FragmentAddTaskBinding
-import kotlinx.android.synthetic.main.add_task_body.view.*
-import java.text.SimpleDateFormat
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog
 import java.util.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
-class AddTaskFragment : Fragment() {
+class AddTaskFragment : Fragment(), TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
 
     private lateinit var addTaskBinding: FragmentAddTaskBinding
     private var isStart:Boolean = false
@@ -47,8 +41,24 @@ class AddTaskFragment : Fragment() {
 
         if (!isStart){
             addTaskBinding.addTaskBody.endTimeCard.visibility = View.VISIBLE
-            addTaskBinding.chooseFab.setImageDrawable(context?.let {getDrawable(it,R.drawable.ic_save_black_24dp) })
+            addTaskBinding.chooseFab.setImageDrawable(context?.let {getDrawable(it, R.drawable.ic_save_black_24dp) })
             addTaskBinding.addTaskBody.startTimeChangeButton.visibility = View.VISIBLE
+        }
+
+        //Set on Click listener to the change button
+        addTaskBinding.addTaskBody.startTimeChangeButton.setOnClickListener {
+            val now = Calendar.getInstance()
+            val dpd =
+                DatePickerDialog.newInstance(
+                    this,
+                    now[Calendar.YEAR],  // Initial year selection
+                    now[Calendar.MONTH],  // Initial month selection
+                    now[Calendar.DAY_OF_MONTH] // Inital day selection
+                )
+            TimePickerDialog.newInstance(
+                this,now[Calendar.HOUR],now[Calendar.MINUTE],false
+            )
+            dpd.show(activity?.supportFragmentManager!!, "Datepickerdialog")
         }
         return addTaskBinding.root
     }
@@ -59,4 +69,16 @@ class AddTaskFragment : Fragment() {
         addTaskViewModel.getTime()
         addTaskViewModel.getDate()
     }
+
+    override fun onTimeSet(view: TimePickerDialog?, hourOfDay: Int, minute: Int, second: Int) {
+
+
+    }
+
+    override fun onDateSet(view: DatePickerDialog?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+
+
+    }
+
+
 }
