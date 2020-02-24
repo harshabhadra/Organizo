ced0063633cc9b32d5af4f0409ae73a809332fc5
harshabhadra <harshasharkey@gmail.com>
2020-02-24 20:39:21 +0530

    Adding Room Database, Entity class for single task added

--- app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskFragment.kt
+++ app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckedTextView
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.harshabhadra.organizo.R
@@ -14,6 +16,7 @@ import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.properties.Delegates
 
 
 /**
@@ -27,6 +30,7 @@ class AddTaskFragment : Fragment(), TimePickerDialog.OnTimeSetListener, DatePick
     private lateinit var addTaskViewModel: AddTaskViewModel
     private var startTimeButtonClicked = false
     private var startDateButtonClicked = false
+    private var isChecked: Boolean? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,43 +47,48 @@ class AddTaskFragment : Fragment(), TimePickerDialog.OnTimeSetListener, DatePick
         //Check what to do Start or save task
         isStart = arguments?.getBoolean("isStart")!!
 
+        //Check if the add end card checkbox is checked or not
+        isChecked = addTaskBinding.addTaskBody.checkedTextView.isChecked
+
         if (!isStart){
-            addTaskBinding.addTaskBody.endTimeCard.visibility = View.VISIBLE
+            addTaskBinding.addTaskBody.checkedTextView.visibility=View.VISIBLE
             addTaskBinding.chooseFab.setImageDrawable(context?.let {getDrawable(it, R.drawable.ic_save_black_24dp) })
             addTaskBinding.addTaskBody.startTimeChangeButton.visibility = View.VISIBLE
             addTaskBinding.addTaskBody.startDateChangeButton.visibility = View.VISIBLE
         }
 
-        //Set on Click listener to the change button
+        //Set on Click listener to the change buttons
         addTaskBinding.addTaskBody.startTimeChangeButton.setOnClickListener(this)
         addTaskBinding.addTaskBody.startDateChangeButton.setOnClickListener(this)
         addTaskBinding.addTaskBody.changeEndTimeButton.setOnClickListener(this)
         addTaskBinding.addTaskBody.endDateChangeButton.setOnClickListener(this)
+        addTaskBinding.addTaskBody.checkedTextView.setOnClickListener(this)
         return addTaskBinding.root
     }
 
     override fun onStart() {
         super.onStart()
         addTaskBinding.addTaskViewModel = addTaskViewModel
-        addTaskViewModel.getTime()
         addTaskViewModel.getDate()
+        addTaskViewModel.getTime()
     }
 
     override fun onTimeSet(view: TimePickerDialog?, hourOfDay: Int, minute: Int, second: Int) {
 
         if (startTimeButtonClicked){
-            addTaskBinding.addTaskBody.startTaskTimeTv.text = "$hourOfDay : $minute"
+            addTaskBinding.addTaskBody.startTaskTimeTv.text = addTaskViewModel.changeTimeFormat(hourOfDay,minute)
         }else{
-            addTaskBinding.addTaskBody.endTaskTimeTv.text = "$hourOfDay : $minute"
+            addTaskBinding.addTaskBody.endTaskTimeTv.text = addTaskViewModel.changeTimeFormat(hourOfDay,minute)
         }
     }
 
     override fun onDateSet(view: DatePickerDialog?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
-
-        if (startDateButtonClicked){
-            addTaskBinding.addTaskBody.startTaskDateTv.text = changeDateFormat(dayOfMonth,monthOfYear,year)
+        if (startDateButtonClicked) {
+            addTaskBinding.addTaskBody.startTaskDateTv.text =
+                addTaskViewModel.changeDateFormat(dayOfMonth, monthOfYear, year)
         }else{
-            addTaskBinding.addTaskBody.endTaskDateTv.text = changeDateFormat(dayOfMonth,monthOfYear,year)
+            addTaskBinding.addTaskBody.endTaskDateTv.text =
+                addTaskViewModel.changeDateFormat(dayOfMonth,monthOfYear,year)
         }
     }
 
@@ -101,11 +110,28 @@ class AddTaskFragment : Fragment(), TimePickerDialog.OnTimeSetListener, DatePick
                 startDateButtonClicked = false
                 openDatePickerDialog()
             }
+            addTaskBinding.addTaskBody.checkedTextView -> {
+                if (!isChecked!!) {
+                    addTaskBinding.addTaskBody.checkedTextView.checkMarkDrawable =
+                        ContextCompat.getDrawable(
+                            requireContext(), R.drawable.ic_check_box_blue_24dp
+                        )
+                    addTaskBinding.addTaskBody.endTimeCard.visibility = View.VISIBLE
+                    isChecked = true
+                }else{
+                    addTaskBinding.addTaskBody.checkedTextView.checkMarkDrawable =
+                        ContextCompat.getDrawable(
+                            requireContext(), R.drawable.ic_check_box_outline_blank_blue_24dp
+                        )
+                    addTaskBinding.addTaskBody.endTimeCard.visibility = View.INVISIBLE
+                    isChecked = false
+                }
+            }
         }
     }
 
     //Open Date picker Dialog
-    fun openDatePickerDialog(){
+    private fun openDatePickerDialog(){
         val now = Calendar.getInstance()
         val dpd =
             DatePickerDialog.newInstance(
@@ -118,7 +144,7 @@ class AddTaskFragment : Fragment(), TimePickerDialog.OnTimeSetListener, DatePick
     }
 
     //Open time picker Dialgo
-    fun openTimePickerDialog(){
+    private fun openTimePickerDialog(){
         val now = Calendar.getInstance()
         val dpd =
             TimePickerDialog.newInstance(
@@ -127,10 +153,5 @@ class AddTaskFragment : Fragment(), TimePickerDialog.OnTimeSetListener, DatePick
         dpd.show(activity?.supportFragmentManager!!, "Datepickerdialog")
     }
 
-    //Format Date
-    fun changeDateFormat(day:Int, month:Int, year:Int):String{
-        val format = SimpleDateFormat("E, dd MMM yyyy", Locale.getDefault())
-        val date = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).parse("$day/$month/$year")
-        return format.format(date)
-    }
+
 }
--- app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskViewModel.kt
+++ app/src/main/java/com/harshabhadra/organizo/ui/userTask/AddTaskViewModel.kt
@@ -1,6 +1,7 @@
 package com.harshabhadra.organizo.ui.userTask
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -11,11 +12,12 @@ import java.util.*
 
 class AddTaskViewModel(private val context: Application) : AndroidViewModel(context) {
 
-
+    //Current Date String
     private var _dateString = MutableLiveData<String>()
     val dateString: LiveData<String>
         get() = _dateString
 
+    //Current Time String
     private var _timeString = MutableLiveData<String>()
     val timeString: LiveData<String>
         get() = _timeString
@@ -36,7 +38,24 @@ class AddTaskViewModel(private val context: Application) : AndroidViewModel(cont
         return formatter.format(this)
     }
 
+    //Get Current Time
     private fun getCurrentDateTime(): Date {
         return Calendar.getInstance().time
+    }
+
+    //Format Date
+     fun changeDateFormat(day:Int, month:Int, year:Int):String{
+
+        val calender = Calendar.getInstance()
+        calender.set(year,month,day)
+        val d = calender.time
+        _dateString.value = d.toString("E, dd MMM yyyy")
+        return d.toString("E, dd MMM yyyy")
+    }
+
+    fun changeTimeFormat(hour:Int, min:Int):String{
+        val date = SimpleDateFormat("HH:mm", Locale.getDefault()).parse("$hour:$min")
+        _timeString.value = date.toString("hh:mm aa")
+        return date.toString("hh:mm aa")
     }
 }
